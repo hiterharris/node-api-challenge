ed1c7b6ea5896aeb77e73c21eb5906d99a6f10f8
Hiter Harris <hiterharris@comcast.net>
2020-02-14 11:38:14 -0500

    CRUD operations for actions complete

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -16,4 +16,49 @@ router.get('/', (req, res) => {
       })
   });
 
+  router.get('/:id', (req, res) => {
+    actionsDb.get(req.params.id)
+      .then(action => {
+          res.status(200).json(action);
+      })
+      .catch(error => {
+        res.status(500).json({message: 'Error'});
+      })
+  });
+
+  router.post('/', (req, res) => {
+    actionsDb.insert(req.body)
+      .then(action => {
+        if(req.body.project_id) {
+            res.status(200).json(action);
+        } else {
+        res.status(500).json({message: 'Error'});
+
+        }
+      })
+      .catch(error => {
+        res.status(500).json({message: 'Error'});
+      })
+  });
+
+  router.put('/:id', (req, res) => {
+    actionsDb.update(req.params.id, req.body)
+      .then(update => {
+          res.status(200).json(update);
+      })
+      .catch(error => {
+        res.status(500).json({message: 'Error'});
+      })
+  });
+
+  router.delete('/:id', (req, res) => {
+    actionsDb.remove(req.params.id)
+      .then(remove => {
+          res.status(200).json(remove);
+      })
+      .catch(error => {
+        res.status(500).json({message: 'Error'});
+      })
+  })
+
 module.exports = router;
